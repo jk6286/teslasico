945af09ff2fd113af10f702afdb0f1558e1aa673
jk6286 <shk6286@gmail.com>
2024-07-04 08:58:23 +0900

    24Y 2Q Production and Deliverly Update

--- src/stores/chartData.js
+++ src/stores/chartData.js
@@ -38,6 +38,7 @@ export const useChartDataStore = defineStore('chartData', () => {
     { year: 2023, quarter: 4, amount: { production: 494989, delivery: 484507 } },
     
     { year: 2024, quarter: 1, amount: { production: 433371, delivery: 386810 } },
+    { year: 2024, quarter: 2, amount: { production: 410831, delivery: 443956 } },
   ];
 
   const 분기별_생산_데이터 = 분기별_생산배송_데이터.map(({ amount }) => amount.production);
