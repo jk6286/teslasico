350dbcac5c0de29b71964081c3bcf6edace9fc0f
jk6286 <shk6286@gmail.com>
2025-04-04 23:30:58 +0900

    Update chartData.js

--- src/stores/chartData.js
+++ src/stores/chartData.js
@@ -41,6 +41,8 @@ export const useChartDataStore = defineStore('chartData', () => {
     { year: 2024, quarter: 2, amount: { production: 410831, delivery: 443956 } },
     { year: 2024, quarter: 3, amount: { production: 469796, delivery: 462890 } },
     { year: 2024, quarter: 4, amount: { production: 459445, delivery: 495570 } },
+    
+    { year: 2025, quarter: 1, amount: { production: 362615, delivery: 336681 } },
   ];
 
   const 분기별_생산_데이터 = 분기별_생산배송_데이터.map(({ amount }) => amount.production);
